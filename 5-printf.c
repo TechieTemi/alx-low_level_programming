c3eae87b91b06ac3fec8bfe5ce629395a646675b
TechiTemi <apparelreepublik@gmail.com>
2023-05-18 17:31:26 +0100

    0x00-hello_world

---
+++ 5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - print a C program followed by a new line with printf function
+*
+*Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
