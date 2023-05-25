30d78f85e328ec924559fe7c47a69c90b8ccaab9
TechiTemi <apparelreepublik@gmail.com>
2023-05-25 03:31:09 +0100

    time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,24 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
-
-            if (column != 0)
-            {
-                _putchar(',');
-                _putchar(' ');
-            }
-
-            if (product < 10 && column != 0)
-                _putchar(' ');
-
-            if (product >= 10)
-                _putchar(product / 10 + '0');
-
-            _putchar(product % 10 + '0');
-        }
-        _putchar('\n');
-    }
+	for (row = 0; row <= 9; row++)
+	{
+	for (column = 0; column <= 9; column++)
+	{
+	product = row * column;
+	if (column != 0)
+	{
+	_putchar(',');
+	_putchar(' ');
+	}
+	if (product < 10 && column != 0)
+	_putchar(' ');
+	if (product >= 10)
+	_putchar(product / 10 + '0');
+	_putchar(product % 10 + '0');
+	}
+	_putchar('\n');
+	}
 }
-
