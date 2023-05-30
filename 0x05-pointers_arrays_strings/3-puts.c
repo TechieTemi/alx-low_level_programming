a362af92b63caa840f0f44bf583c5d0fc2840d37
TechiTemi <apparelreepublik@gmail.com>
2023-05-30 03:13:57 +0100

    _puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,11 @@
+#include "stdio.h"
+
+/**
+*_puts -  A function that prints a string, to stdout.
+*@str: Takes str which is a pointer to char as a parameter.
+*/
+
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
