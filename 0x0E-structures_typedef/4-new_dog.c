5bc0404018103700ba4890463d6a216179f064dd
dorcascoding <dorcasodewole@gmail.com>
2023-06-20 02:58:59 +0100

    This is my project about the concept of structure and typedef in C

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,23 +1,50 @@
 #include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * init_dog - initialize dog struct
- *
- * @d: input pointer to struct dog
- * @name: input for dog name
- * @age: input for dog age
- * @owner: input for dog owner
- *
- * Return: nothing
+ * new_dog - new dog
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: newdog
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner)
+dog_t *new_dog(char *name, float age, char *owner)
 {
-if (d)
-{
-      /*d->name = name;*/
-(*d).name = name;
-(*d).age = age;
-(*d).owner = owner;
-}
+
+	int i = 0, j = 0, k;
+	dog_t *doge;
+
+	while (name[i] != '\0')
+		i++;
+	while (owner[j] != '\0')
+		j++;
+	doge = malloc(sizeof(dog_t));
+	if (doge == NULL)
+	{
+		free(doge);
+		return (NULL);
+	}
+	doge->name = malloc(i * sizeof(doge->name));
+	if (doge->name == NULL)
+	{
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= i; k++)
+		doge->name[k] = name[k];
+	doge->age = age;
+	doge->owner = malloc(j * sizeof(doge->owner));
+	if (doge->owner == NULL)
+	{
+		free(doge->owner);
+		free(doge->name);
+		free(doge);
+		return (NULL);
+	}
+	for (k = 0; k <= j; k++)
+		doge->owner[k] = owner[k];
+	return (doge);
 }
