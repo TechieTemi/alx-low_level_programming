eef2c76a5129bfc5c82daba9d26952dd0f93e554
TechieTemi <apparelreepublik@gmail.com>
2023-06-27 11:01:46 +0100

    function that prints pattern of triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+*print_triangle - prints a triagle, followed by a new line
+*@size: size of the triangle
+*/
+
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int j, i;
+for (j = 1; j <= size; j++)
+{
+for (i = j; i < size; i++)
+{
+_putchar(' ');
+}
+for (i = 1; i <= j; i++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
