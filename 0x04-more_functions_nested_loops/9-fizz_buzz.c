fe34fa965ae9d607a770b02bcd029f7bd9a21bc2
TechieTemi <apparelreepublik@gmail.com>
2023-06-27 11:11:46 +0100

    function that prints pattern of triangle

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,29 +8,29 @@
  */
 int main(void)
 {
-	int i;
+int n;
 
-	for (i = 1; i <= 100; i++)
+	for (n = 1; n <= 100; n++)
 	{
-		if (i % 3 == 0 && i % 5 != 0)
-		{
-			printf(" Fizz");
-		} else if (i % 5 == 0 && i % 3 != 0)
-		{
-			printf(" Buzz");
-		} else if (i % 3 == 0 && i % 5 == 0)
+		if (n == 1)
+			printf("%d", n);
+		else if ((n % 3 == 0) && (n % 5 == 0))
 		{
 			printf(" FizzBuzz");
-		} else if (i == 1)
+		}
+		else if (n % 3 == 0)
 		{
-			printf(" %d", i);
-		} else
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
 		{
-			printf(" %d", i);
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
 		}
 	}
-
 	printf("\n");
-
 	return (0);
 }
