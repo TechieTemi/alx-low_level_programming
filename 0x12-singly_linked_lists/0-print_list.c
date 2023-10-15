47ff043307f199b979a2df6a9bcd7ee7a5bedc02
TechieTemi <apparelreepublik@gmail.com>
2023-10-16 00:28:13 +0100

    Betty abeg

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,31 +1,24 @@
 #include "lists.h"
 
 /**
- * print_list - A function that print all the element in a list
- * @h: Of type list_h
- * Return: number of node printed
+ * print_list - Prints all elements in a list
+ * @h: A pointer to the list
+ * Return: Number of nodes printed
  */
+size_t print_list(const list_t *h) {
+    const list_t *temp;
+    size_t count = 0;
 
-size_t print_list(const list_t *h)
-{
-	const list_t *temp;
-	size_t count = 0;
-
-	/* what if the list is not empty */
-
-	temp = h;
-	while (temp != NULL)
-	{
-		if (temp->str == NULL)
-		{
-			printf("[0] (nil)\n");
-		}
-		else
-		{
-			printf("[%u] %s\n", temp->len, temp->str);
-		}
-		temp = temp->next;
-		count++;
-	}
-	return (count);
+    temp = h;
+    while (temp != NULL) {
+        if (temp->str == NULL) {
+            printf("[0] (nil)\n");
+        } else {
+            printf("[%u] %s\n", temp->len, temp->str);
+        }
+        temp = temp->next;
+        count++;
+    }
+    return count;
 }
+
