1cd55a27f7e39fbaed218fcca829a7790c6ae5fd
TechieTemi <apparelreepublik@gmail.com>
2023-10-16 13:33:28 +0100

    you are beat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+
+/**
+ *myStartupFun - A function that prints You're beat!, you must allow,
+ *		\nI bore my house upon my back!\n before the main function.
+ */
+
+void myStartupFun (void)
+{
+	printf("You\'re beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
