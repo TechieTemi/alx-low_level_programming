88d4ffc9838ce72dd5a3dea7dcab60c2c98981dc
TechieTemi <apparelreepublik@gmail.com>
2023-10-16 01:24:44 +0100

    betty please

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,20 +5,24 @@
  * @h: A pointer to the list
  * Return: Number of nodes printed
  */
-size_t print_list(const list_t *h) {
-    const list_t *temp;
-    size_t count = 0;
+size_t print_list(const list_t *h)
+{
+	const list_t *temp;
+	size_t count = 0;
 
-    temp = h;
-    while (temp != NULL) {
-        if (temp->str == NULL) {
-            printf("[0] (nil)\n");
-        } else {
-            printf("[%u] %s\n", temp->len, temp->str);
-        }
-        temp = temp->next;
-        count++;
-    }
-    return count;
+	temp = h;
+	while (temp != NULL)
+	{
+		if (temp->str == NULL)
+
+			printf("[0] (nil)\n");
+	}
+	else
+	{
+		printf("[%u] %s\n", temp->len, temp->str);
+	}
+	temp = temp->next;
+	count++;
 }
-
+return (count);
+}
